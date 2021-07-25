01fe1bb68048d3414d9e855c5c01322497b959f7
Natalia K <nataliaandkirik@gmail.com>
2021-07-25 22:29:14 +0300

    change styles in Message component

--- components/Message.js
+++ components/Message.js
@@ -1,52 +1,20 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import styled from "styled-components";
+import {
+  Avatar,
+  Date,
+  ImageHeart,
+  MainContainer,
+  MessageContainer,
+  MessageReplies,
+  MessageTitle,
+  UserName,
+} from "../styled_const/MessageStyledConst";
 
-
-const MessageContainer = styled.View`
-  background-color: white;
-  margin: 7px;
-  flex-direction: row;
-  justify-content: space-around;
-  border-radius: 10px;
-`;
-
-const Avatar = styled.Image`
-  border: 1px solid black;
-`;
-const MainContainer = styled.View`
-  justify-content: space-between;
-  width: 50%;
-  padding: 0 0 0 5px;
-`;
-const MessageTitle = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  color: #222222;
-`;
-const UserName = styled.Text`
-  font-size: 15px;
-  color: #4f5153;
-  text-align: left;
-`;
-const MessageReplies = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-  color: #222222;
-`;
-const ImageHeart = styled.Image`
-  width: 20px;
-  height: 20px;
-  color: ${props => props.color};`;
-const Date = styled.Text`
-  color: #6e7f8d;
-  font-size: 14px;
-`;
 
 export const Message = ({ date, like, name, photo, replies, title }) => {
   return (
     <MessageContainer>
-
       <Avatar source={photo} />
 
       <MainContainer>
---
+++ styled_const/MessageStyledConst.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const MessageContainer = styled.View`
+  background-color: white;
+  margin: 7px;
+  flex-direction: row;
+  justify-content: space-around;
+  border-radius: 10px;
+  padding: 7px 11px;
+`;
+export const Avatar = styled.Image`
+  width: 120px;
+  height: 120px;
+  border-radius: 60px;
+`;
+export const MainContainer = styled.View`
+  justify-content: space-between;
+  width: 46%;
+  margin: 0 0 0 15px;
+`;
+export const MessageTitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #222222;
+`;
+export const UserName = styled.Text`
+  font-size: 17px;
+  color: #4f5153;
+  text-align: left;
+`;
+export const MessageReplies = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #222222;
+`;
+export const ImageHeart = styled.Image`
+  width: 25px;
+  height: 25px;
+  color: ${props => props.color};
+`;
+export const Date = styled.Text`
+  color: #6e7f8d;
+  font-size: 16px;
+`;
