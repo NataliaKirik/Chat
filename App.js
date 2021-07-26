dc9ee864362ffb86738c3f428105e92bb508f600
nataliakirik@gmail.com <SAMsunGGvs37533>
2021-07-26 15:56:48 +0300

    add: bottom tabs; react native vector icons

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from "react";
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import EntypoIcons from 'react-native-vector-icons/Entypo';
 
 import {HomeScreen} from "./screens/HomeScreen";
 import {MessageScreen} from "./screens/MessageScreen";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="Messages" component={MessageScreen}/>
-            </Stack.Navigator>
+            <Tab.Navigator screenOptions={({route}) => ({
+                tabBarIcon: ({color, size}) => {
+                    if (route.name === 'Home') {
+                        return <EntypoIcons name={'home'} size={size} color={color}/>;
+
+                    } else if (route.name === 'Messages') {
+                        return <EntypoIcons name={'message'} size={size} color={color}/>;
+
+                    }
+
+                },
+            })}
+                           tabBarOptions={{
+                               activeTintColor: '#df245e',
+                               inactiveTintColor: 'gray',
+                           }}>
+                <Tab.Screen name="Home" component={HomeScreen}/>
+                <Tab.Screen name="Messages" component={MessageScreen}/>
+            </Tab.Navigator>
         </NavigationContainer>
     );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,10 +6,6 @@ export function HomeScreen({navigation}) {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Home Screen</Text>
-            <Button
-                title="Go to Messages"
-                onPress={() => navigation.navigate('Messages')}
-            />
         </View>
     );
 }
