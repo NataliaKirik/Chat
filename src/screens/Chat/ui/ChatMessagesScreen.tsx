1f1db4b6646c49eb79552b3d251499aacabc60ae
nataliakirik@gmail.com <SAMsunGGvs37533>
2021-08-09 17:19:41 +0300

    add KeyboardAwareScrollView to ChatMessagesScreen

--- src/screens/Chat/ui/ChatMessagesScreen.tsx
+++ src/screens/Chat/ui/ChatMessagesScreen.tsx
@@ -1,21 +1,14 @@
 import React, {FC, useEffect} from "react";
-import {FlatList, KeyboardAvoidingView, Platform, StyleSheet, View} from "react-native";
+import {StyleSheet, View} from "react-native";
 import I18n from "react-native-i18n";
 import {useDispatch, useSelector} from "react-redux";
 import {addChatUserDataTC} from "../bll/chatUserNameReducer";
 import {AppRootStateType} from "../../../app/store";
 import {ChatMessage} from "../../../components/ChatMessage/ChatMessage";
 import {getChatMessageDataTC} from "../bll/chatMessagesReducer";
-import {
-    ChatScreenContainer,
-    InputAndButtonContainer,
-    TextInputStyled,
-    ButtonStyled,
-    AntDesignStyled
-} from "./ChatMessagesScreenStyle"
+import {AntDesignStyled, ButtonStyled, InputAndButtonContainer, TextInputStyled} from "./ChatMessagesScreenStyle"
 import {ChatUserDataType} from "../../../api/api";
 import {KeyboardAwareFlatList, KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
-import {useRef} from 'react';
 
 
 export const ChatMessagesScreen: FC = () => {
@@ -67,33 +60,9 @@ const styles = StyleSheet.create({
         justifyContent: "space-between"
     },
     container: {
-        backgroundColor: '#4c69a5',
         flex: 1
     },
     contentContainerStyle: {}
 })
 
-
-//     <KeyboardAwareScrollView style={styles.container} extraHeight={90} contentContainerStyle={styles.containerStyle}>
-//     <View>
-//     <KeyboardAwareFlatList
-// data={data}
-// renderItem={({item}) => (
-//     <ChatMessage {...item}/>
-// )}
-// keyExtractor={(_, index) => "List-" + index}
-// />
-// </View>
-// <InputAndButtonContainer>
-//     <TextInputStyled
-//         value={message}
-//         onChangeText={message => setMessage(message)}
-//         multiline={true}
-//         placeholder={I18n.t('chatInputMessage')}
-//     />
-//     <ButtonStyled mode="contained" onPress={onPressBtnSend}>
-//         <AntDesignStyled name={"caretright"}/>
-//     </ButtonStyled>
-// </InputAndButtonContainer>
-// </KeyboardAwareScrollView>
 
